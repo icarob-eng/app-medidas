a38ae726f472e6953455205185126b5369b421a3
icaro <ibsc013@gmail.com>
2022-07-17 15:42:42 -0300

    Casos de uso básicos, permissões e Intent

--- app/src/main/java/com/medidaspla/MainActivity.java
+++ app/src/main/java/com/medidaspla/MainActivity.java
@@ -1,14 +1,109 @@
 package com.medidaspla;
 
+import androidx.activity.result.ActivityResultCaller;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraProvider;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Switch;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    // views:
+    private Button btnAnalise;
+    private ImageButton btnPic;
+    private EditText txtPasta, txtArquivo;
+    private PreviewView viewFinder;
+    private Switch switchBordas;
 
+    // solicitador de permissões
+    private ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(), b -> {}
+    );
+
+    // use cases:
+    private ImageCapture imageCapture;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        instantiateViews();
+        btnAnalise.setOnClickListener(v -> startActivity(new Intent(
+                MainActivity.this, AnaliseActivity.class)
+        ));
+
+
+        // solicita permissão para uso de câmera, se não já tiver
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                        != PackageManager.PERMISSION_GRANTED
+        ) requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+
+
+        // tenta inicializar casos de uso
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider
+                .getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+                try {
+                    ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                    startCameraX(cameraProvider);
+                } catch (ExecutionException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }, getMainExecutor());
+
+    }
+
+    private void instantiateViews(){
+        // incializa views
+        txtPasta = findViewById(R.id.textNomePasta);
+        txtArquivo = findViewById(R.id.textNomeArquivo);
+        viewFinder = findViewById(R.id.viewFinder);
+        btnPic = findViewById(R.id.btnPic);
+        btnAnalise = findViewById(R.id.btnAnalise);
+        switchBordas = findViewById(R.id.switchBordas);
+    }
+
+    private void startCameraX(ProcessCameraProvider cameraProvider) {
+        cameraProvider.unbindAll();
+
+        // Camera Selector use case
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        // Preview use case
+        Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
+
+        // Image Capture use case
+        imageCapture = new ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MAXIMIZE_QUALITY)
+                .build();
+
+        cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
     }
 }
